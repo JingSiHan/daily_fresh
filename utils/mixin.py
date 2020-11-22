b7ce80cbf69a39b6bf33c1c43cd9a920143905c8
Spencer <944594380@qq.com>
2020-11-22 14:47:05 +0800

    格式化代码

--- utils/mixin.py
+++ utils/mixin.py
@@ -2,7 +2,6 @@ from django.contrib.auth.decorators import login_required
 
 
 class LoginRequireMixin(object):
-
     @classmethod
     def as_view(cls, **initkwargs):
         view = super(LoginRequireMixin, cls).as_view(**initkwargs)
