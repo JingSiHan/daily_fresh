39b4124fb830e6b5185180086d04298089726000
Spencer <944594380@qq.com>
2020-11-26 23:35:21 +0800

    访问静态页面成功，但是无法通过admin增删内容来重新获取静态页面。下面先做页面数据的缓存。

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,5 +1,41 @@
 from django.contrib import admin
-from apps.goods.models import GoodsType
+from apps.goods.models import GoodsType, IndexPromotionBanner, IndexTypeGoodsBanner, IndexGoodsBanner
+
 # Register your models here.
 
-admin.site.register(GoodsType)
+
+class BaseModelAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        # 新增或更新表中数据时调用
+        super(IndexPromotionBannerAdmin, self).save_model()
+        # 发出任务，让celery　worker重新生成首页静态页面
+        from celery_tasks.tasks import generate_static_index
+        generate_static_index.delay()
+
+    def delete_model(self, request, obj):
+        super(IndexPromotionBannerAdmin, self).delete_model()
+        # 发出任务，让celery　worker重新生成首页静态页面
+        from celery_tasks.tasks import generate_static_index
+        generate_static_index.delay()
+
+
+class IndexPromotionBannerAdmin(BaseModelAdmin):
+    pass
+
+
+class GoodsTypeAdmin(BaseModelAdmin):
+    pass
+
+
+class IndexTypeGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+
+class IndexGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+
+admin.site.register(GoodsType, GoodsTypeAdmin)
+admin.site.register(IndexPromotionBanner, IndexPromotionBannerAdmin)
+admin.site.register(IndexTypeGoodsBanner, IndexTypeGoodsBannerAdmin)
+admin.site.register(IndexGoodsBanner, IndexGoodsBannerAdmin)
