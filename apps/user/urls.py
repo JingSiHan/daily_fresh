cbf1233151b8f7b3052c9d0c1af4a470ae8546f6
JingSiHan <944594380@qq.com>
2020-12-21 22:25:31 +0800

    为user/urls.py添加注释。

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
 
     re_path(r'^active/(?P<token>.*)$', ActiveView.as_view(), name='active'),  # 激活账户
     re_path(r'^login$', LoginView.as_view(), name='login'),  # 激活成功，显示登录页面
-    re_path(r'^info$', UserInfoView.as_view(), name='info'),
-    re_path(r'^order$', UserOrderView.as_view(), name='order'),
-    re_path(r'^address$', AddressView.as_view(), name='address'),
+    re_path(r'^info$', UserInfoView.as_view(), name='info'),  # 用户中心-信息页
+    re_path(r'^order$', UserOrderView.as_view(), name='order'),  # 用户中心-订单页
+    re_path(r'^address$', AddressView.as_view(), name='address'),  # 用户中心-地址页
 
 ]
